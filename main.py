0cc522a6200997c3f2276962b774f30dab12d3be
Siskaisn <siskaisnaini24@gmail.com>
2024-09-21 15:15:20 +0700

    latihan ngoding day 2 pelcode

---
+++ main.py
@@ -0,0 +1,11 @@
+nilai = int(input("masukan nilai: "))
+
+if ( ): 
+    print("Selamat anda lulus dengan nilai yang memuaskan")
+elif(nilai > 80):
+    print ("selamat anda lulus")
+else:
+    print("anda masih belum lulus tes semangat dan coba lagi")
+
+
+
